e08eb6baf0ba5b660a549e7ab563a88b0dce4328
Barbiesmile <barbie850831@yahoo.com.tw>
2025-07-09 22:45:09 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontSize: "20px", padding: "2rem" }}>
+      ✅ LINE API 已成功部署<br />
+      可從 Airtable 按鈕呼叫 /api/send 傳送訊息
+    </main>
+  );
+}
