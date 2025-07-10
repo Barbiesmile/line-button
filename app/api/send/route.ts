eae5a8c7c379da1909574ed63265d254db7db6bc
Barbiesmile <barbie850831@yahoo.com.tw>
2025-07-10 14:26:36 +0800

    Update route.ts
    moon big smile

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -76,7 +76,7 @@ async function handleSendMessage(userId: string, date: string) {
   const accessToken = LINE_TOKENS[lineAccount] || LINE_TOKENS["ltf8289j"]; // 預設主帳號
 
   const time = extractTimeFromDate(date);
-  const message = `嗨嗨~🔉預約提醒通知\n我們明天 ${time} 見唷🌝🌝`;
+  const message = `嗨嗨~🔉預約提醒通知\n我們明天 ${time} 見唷(moon big smile)(moon big smile)`;
 
   const lineRes = await sendLineMessage(userId, message, accessToken);
   if (!lineRes.ok) {
